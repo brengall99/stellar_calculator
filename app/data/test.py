9dd9020ead3c676ac6e32c6558a956459a084faa
Brendan Gallagher <159880330+brengall99@users.noreply.github.com>
2024-02-15 20:06:56 +0000

    Delete app/data/test.py

--- app/data/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('ROOT.csv')
-
-def get_plan(arpu):
-    return min(common_list, key=lambda x:abs(x-float(arpu)))
-    
-df["Suggested plan"] = df["ARPU"].apply(get_plan)
-df.to_csv('ROOT.csv')
